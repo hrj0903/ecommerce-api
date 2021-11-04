24f44232808a5639c2cd1dcf442a51351f0f8b34
hrj0903 <heerij93@gmail.com>
2021-11-04 19:44:35 +0900

    add auth and jwt

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.js';
-
+import authRoute from './routes/auth.js';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
@@ -12,7 +13,9 @@ mongoose
   .then(() => console.log('DBConnection Successfull!'))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 
 app.listen(process.env.port || 8000, () => {
